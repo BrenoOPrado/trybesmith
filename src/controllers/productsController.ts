f6caf326f3e6205c8a9522f3f15d7b8336a50376
BrenoOPrado <breno.o.prado@hotmail.com>
2022-11-07 17:15:22 -0300

    começando a corrigir :turtle:

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -3,15 +3,18 @@ import { IProduct } from '../interfaces';
 import ProductsService from '../services/productsService';
 
 export default class ProductsController {
-  service = new ProductsService();
+  constructor(private service = new ProductsService()) {}
 
   async insert(req: Request, res: Response): Promise<void> {
+    console.log('insert do controller');
+    console.log(this.service);
+    
     const result: IProduct = await this.service.insert(req.body);
   
     res.status(201).json(result);
   }
 
-  async getAll(req: Request, res: Response): Promise<void> {
+  async getAll(_req: Request, res: Response): Promise<void> {
     const result: IProduct[] = await this.service.getAll();
     
     res.status(200).json(result);
--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -4,15 +4,18 @@ import validateBody from '../middlewares/validateBody';
 import ProductsModel from '../models/productsModel';
 
 export default class ProductsService {
-  model = new ProductsModel();
+  constructor(private model = new ProductsModel()) {}
 
   async insert(body:IProduct): Promise<IProduct> {
+    console.log('entrou no service');
+    
     const schema = Joi.object<IProduct>({
       name: Joi.string().required(),
       amount: Joi.string().required(),
     });
     validateBody(body, schema);
-  
+    console.log('insert do service');
+    
     const result:IProduct = await this.model.insert(body);
   
     return result;
